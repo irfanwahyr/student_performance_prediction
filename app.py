f8ec7b6e3b19d2451718781fb328a953e186ebf5
Reinz_IWR <71551362+irfanwahyr@users.noreply.github.com>
2024-05-23 20:02:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,34 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-loaded_kmeans = joblib.load('kmeans_model.joblib')
+loaded_kmeans = joblib.load('decisiontree_model.joblib')
 
 def predict_cluster(data):
     predicted_cluster = loaded_kmeans.predict(data)
     return predicted_cluster[0]
 
+def predict_outcome(predict):
+    if predict == 0:
+        return 'Mahasiswa diprediksi akan Graduate'
+    else:
+        return 'Mahasiswa diprediksi akan Dropout'
+
 st.title('Prediksi Cluster Mahasiswa')
 
-age = st.number_input('Umur saat mendaftar', min_value=0, max_value=100, value=13)
-gender = st.selectbox('Jenis Kelamin', [0, 1], format_func=lambda x: 'Perempuan' if x == 0 else 'Laki-laki')
+input_data = {
+    'memiliki Hutang?': st.radio('Debtor', options=['Tidak', 'Ya']),
+    'Biaya Kuliah terkini?': st.radio('Biaya Kuliah Terkini', options=['Tidak', 'Ya']),
+    'Tingkat Pengangguran': st.number_input('Tingkat Pengangguran', min_value=0.0, value=50.0),
+    'Inflation_rate': st.number_input('Inflation Rate', value=0.0),
+    'GDP': st.number_input('GDP', value=0.0)
+}
 
-input_data = pd.DataFrame({
-    'Age_at_enrollment': [age],
-    'Gender': [gender]
-})
+input_data['memiliki Hutang?'] = 1 if input_data['memiliki Hutang?'] == 'Ya' else 0
+input_data['Biaya Kuliah terkini?'] = 1 if input_data['Biaya Kuliah terkini?'] == 'Ya' else 0
 
-features = ['Age_at_enrollment', 'Gender']
-X = input_data[features]
+input_df = pd.DataFrame(input_data, index=[0])
 
 if st.button('Prediksi'):
-    predicted_cluster = predict_cluster(X)
-    st.write(f"Cluster untuk Mahasiswa: {predicted_cluster}")
+    predicted = predict_cluster(input_df)
+    prediction = predict_outcome(predicted)
+    st.write(prediction)
